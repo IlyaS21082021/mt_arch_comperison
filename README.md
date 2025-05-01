c5d4fdb4960d38b3064bfcee47ba41bf261a89f7
Ilya Selin <i.selin.moscow@gmail.com>
2025-05-01 20:44:45 +0300

    tests

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ Comparison of two architectures in multithread application:
 1, One queue for many threads   (DG_arch)
 2. Many queues distributed among the threads (my_arch)
 
+RESULT with buffer size 100000000
+4 threads:
+DG_arch computing time: 1700405 mcs
+my_arch computing time: 1418179 mcs
+
+7 threads:
+DG_arch computing time: 2194381 mcs
+my_arch computing time: 1691384 mcs
+
+10 threads:
+DG_arch computing time: 3097362 mcs
+my_arch computing time: 3927116 mcs
+
+12 threads:
+DG_arch computing time: 3584725 mcs
+my_arch computing time: 4699418 mcs
+
+
+
+
--- my_arch/main.cpp
+++ my_arch/main.cpp
@@ -4,14 +4,14 @@
 
 int main(int argc, char* argv[])
 {
-    /*if (argc < 3)
+    if (argc < 3)
     {
         std::cerr << "Too few parameters\n";
         return 0;
-    }*/
-    //TMclass MXor(std::stoi(argv[2]), std::stoi(argv[1]));
+    }
+    TMclass MXor(std::stoi(argv[2]), std::stoi(argv[1]));
     auto start = std::chrono::high_resolution_clock::now();
-    TMclass MXor(10, 100000000);
+    //TMclass MXor(10, 100000000);
     MXor.Run();
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
